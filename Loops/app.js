fc781f4cffc1f57d5d94e7837273423eda5ab161
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-06 23:57:19 +0530

    loops

--- Loops/app.js
+++ Loops/app.js
@@ -1,4 +1,11 @@
 //to print numbers from 1-100
-for (i = 1; i<=100; i++){
+console.log("Using For Loop")
+for (i = 1; i<=10; i++){
+    console.log(i);
+}
+
+//Reverse print
+console.log("Reverse print")
+for(i = 5; i>=0; i--){
     console.log(i);
 }
