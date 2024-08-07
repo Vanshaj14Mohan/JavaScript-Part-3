beea6eb0c19736a5cdd8bc2efd9a189e2ee99303
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-08 00:36:38 +0530

    tasks

--- Loops/app.js
+++ Loops/app.js
@@ -6,6 +6,19 @@ for (i = 1; i<=10; i++){
 
 //Reverse print
 console.log("Reverse print")
-for(i = 5; i>=0; i--){
-    console.log(i);
-}
+// for(i = 5; i>=0; i--){
+//     console.log(i);
+// }
+
+console.log("Task")
+//print odd numbers from 1-15.
+// console.log("Odd Numbers")
+// for(i=1; i<=15; i+2){
+//     console.log(i);
+// }
+
+//For even numbers
+console.log("Even numbers")
+// for(i =2; i<=20; i +2){
+//     console.log(i);
+// }
