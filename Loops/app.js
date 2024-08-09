b30878e60ad81eec1ea018fc4c3b999186069f03
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-09 23:19:21 +0530

    Files

--- Loops/app.js
+++ Loops/app.js
@@ -6,19 +6,43 @@ for (i = 1; i<=10; i++){
 
 //Reverse print
 console.log("Reverse print")
-// for(i = 5; i>=0; i--){
-//     console.log(i);
-// }
+for(i = 5; i>=0; i--){
+    console.log(i);
+}
 
-console.log("Task")
-//print odd numbers from 1-15.
-// console.log("Odd Numbers")
-// for(i=1; i<=15; i+2){
-//     console.log(i);
-// }
+//Print odd numbers from 1-15
+console.log("Odd numbers")
+for(let i =1; i<=15; i =i+2){
+    console.log(i);
+}
 
-//For even numbers
+//for reverse case
+console.log("reverse odd case")
+for(let i =15; i>=1; i = i-2){
+    console.log(i);
+}
+
+//Print even numbers from 2-20
 console.log("Even numbers")
-// for(i =2; i<=20; i +2){
+for(let i =2; i<=20; i = i+2){
+    console.log(i);
+}
+
+//for reverse case
+console.log("Reverse even case")
+for(let i = 20; i>=2; i = i-2){
+    console.log(i);
+}
+
+//Print multiplication table
+// console.log("Table of 5");
+// for(let i =5; i<=50; i = i+5){
 //     console.log(i);
-// }
+// } or another way to write this
+console.log("Table of any number")
+let n = prompt("Enter a number");
+n = parseInt(n);
+for(let i =n; i<=n*10; i=i+n){
+    console.log(i);
+}
+
