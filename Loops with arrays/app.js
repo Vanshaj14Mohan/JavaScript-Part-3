43ae23aba0a160f5e019f9c9f13b31a62ba9e844
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-17 23:50:36 +0530

    Loops with array

--- Loops with arrays/app.js
+++ Loops with arrays/app.js
@@ -4,6 +4,18 @@ for(i =0; i<fruits.length; i++){
     console.log(i, fruits[i]);
 }
 
+//for odd case ->
+// for(i =1; i<fruits.length; i = i+2){
+//     console.log(i, fruits[i]);
+// }
+
+//for even case ->
+// for(i =0; i<fruits.length; i = i+2){
+//     console.log(i, fruits[i]);
+// }
+
+
+
 //for reverse case
 // for(i =fruits.length-1; i>=0; i--){
 //     console.log(i, fruits[i]);
