89c021fe8a54fa393bb21a42630d2119c9a570fe
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-17 23:46:18 +0530

    Loops with arrays

---
+++ Loops with arrays/app.js
@@ -0,0 +1,10 @@
+let fruits = ["mango", "apple", "banana", "litchi", "orange", "peach", "guava"];
+
+for(i =0; i<fruits.length; i++){
+    console.log(i, fruits[i]);
+}
+
+//for reverse case
+// for(i =fruits.length-1; i>=0; i--){
+//     console.log(i, fruits[i]);
+// }
