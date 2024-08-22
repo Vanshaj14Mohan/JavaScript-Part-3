185ea67222db6285f8bd25b6ad6362fd3effb926
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-22 23:18:20 +0530

    nested loops with array

--- Loops with arrays/app.js
+++ Loops with arrays/app.js
@@ -20,3 +20,21 @@ for(i =0; i<fruits.length; i++){
 // for(i =fruits.length-1; i>=0; i--){
 //     console.log(i, fruits[i]);
 // }
+
+console.log("Nested arrays");
+let numbs = [["ab", "cd", "ef"], ["gh", "ij", "kl"]];
+for(let i =0; i<numbs.length; i++){
+    console.log(numbs[i]);
+    for(let j =0; j<numbs[i].length; j++){
+        console.log(`j = ${j}, ${numbs[i][j]}`);
+    }
+}  
+
+let students = [["Alex", 90], ["Matt", 92], ["George", 91]];
+for(let i =0 ; i<students.length; i++){
+    console.log(`Info of students: #${i}`);
+    //for values
+    for(let j = 0; j<students[i].length; j++){
+        console.log(students[i][j]);
+    }
+}
